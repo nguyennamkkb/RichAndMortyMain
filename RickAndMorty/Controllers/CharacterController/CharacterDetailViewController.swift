086d362d6e50342ccfaf7af16b32a6d6845f39b5
namnl <>
2023-04-04 08:09:54 +0700

    detail

--- RickAndMorty/Controllers/CharacterController/CharacterDetailViewController.swift
+++ RickAndMorty/Controllers/CharacterController/CharacterDetailViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class CharacterDetailViewController: UIViewController {
 
+    @IBOutlet var characterImage: UIImageView!
+    
+    @IBOutlet var characterName: UILabel!
+    @IBOutlet var characterStatus: UILabel!
+    @IBOutlet var characterLocation: UILabel!
+    @IBOutlet var characterFirstSeen: UILabel!
+    
+    init(characterImage: UIImageView!, characterName: UILabel!, characterStatus: UILabel!, characterLocation: UILabel!, characterFirstSeen: UILabel!) {
+        self.characterImage = characterImage
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterLocation = characterLocation
+        self.characterFirstSeen = characterFirstSeen
+        super.init(nibName: "CharacterDetailViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Detail Character"
         // Do any additional setup after loading the view.
     }
-
+    
+ 
 
     /*
     // MARK: - Navigation
--- RickAndMorty/Controllers/CharacterController/CharacterViewController.swift
+++ RickAndMorty/Controllers/CharacterController/CharacterViewController.swift
@@ -9,93 +9,115 @@ import UIKit
 import ObjectMapper
 import Kingfisher
 class CharacterViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
-  var listCharacter = [RMCharacter]()
-  var info = Info()
-  var page: Int = 1
-  var titleController: String?
-  @IBOutlet var collectionView: UICollectionView!
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    collectionView.delegate = self
-    collectionView.dataSource = self
-    let nib = UINib(nibName: "CharacterCollectionViewCell", bundle: .main)
-    collectionView.register(nib, forCellWithReuseIdentifier: "cell")
-    // Do any additional setup after loading the view.
-    setLayout()
-    getFirstCharacters()
-  }
-
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return listCharacter.count
-  }
-
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CharacterCollectionViewCell else {return UICollectionViewCell()}
-    let item = listCharacter[indexPath.row]
-    cell.layer.cornerRadius = 5.0
-    cell.layer.masksToBounds = true
-    cell.layer.borderWidth = 0.1
-
-    cell.backgroundView?.layer.shadowColor = UIColor.black.cgColor
-    cell.backgroundView?.layer.shadowRadius = 5
-    cell.backgroundView?.layer.shadowOpacity = 1
-    cell.backgroundView?.layer.shadowOffset = CGSize(width: 2, height: 2)
-
-
-    cell.nameCharacter.text = item.name
-    cell.statusCharacter.text = "\(item.status ?? "unknown") - \(item.species ??  "unknown")"
-    cell.imageCharacter.kf.setImage(with: URL(string: item.image ?? ""))
-
-    cell.data = item
-    return cell
-  }
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    //    print(listCharacter[indexPath.row].toJSON())
-  }
-  func setLayout(){
-    let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-    let width = Constants.screenWidth / 2 - 7.5
-    let height = 300
-    layout.minimumInteritemSpacing = 2.5
-    layout.minimumLineSpacing = 5
-    layout.itemSize.width = width
-    layout.itemSize.height = CGFloat(height)
-
-    collectionView.setCollectionViewLayout(layout, animated: true)
-  }
-  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-    if indexPath.row == collectionView.numberOfItems(inSection: indexPath.section) - 1 {
-      updateNext()
+    
+    var listCharacter = [RMCharacter]()
+    var info = Info()
+    var page: Int = 1
+    var titleController: String?
+    @IBOutlet var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: "CharacterCollectionViewCell", bundle: .main)
+        collectionView.register(nib, forCellWithReuseIdentifier: "cell")
+        // Do any additional setup after loading the view.
+        setLayout()
+        getFirstCharacters()
     }
-  }
-  func getFirstCharacters(){
-      ServiceManager.common.getCharacters(param: nil){
-      (response) in
-      if response?.results != nil {
-        guard let character =  Mapper<RMCharacter>().mapArray(JSONObject: response?.results) else {return}
-        self.listCharacter = character
-        self.info = response?.info ?? Info()
-        DispatchQueue.main.async {
-          self.collectionView.reloadData()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listCharacter.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CharacterCollectionViewCell else {return UICollectionViewCell()}
+        let item = listCharacter[indexPath.row]
+        cell.layer.cornerRadius = 5.0
+        cell.layer.masksToBounds = true
+        cell.layer.borderWidth = 0.1
+        
+        cell.backgroundView?.layer.shadowColor = UIColor.black.cgColor
+        cell.backgroundView?.layer.shadowRadius = 5
+        cell.backgroundView?.layer.shadowOpacity = 1
+        cell.backgroundView?.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        
+        cell.nameCharacter.text = item.name
+        cell.statusCharacter.text = "\(item.status ?? "unknown") - \(item.species ??  "unknown")"
+        cell.imageCharacter.kf.setImage(with: URL(string: item.image ?? ""))
+        
+        cell.data = item
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //    print(listCharacter[indexPath.row].toJSON())
+        let item = listCharacter[indexPath.row]
+        let vc = CharacterDetailViewController(
+            characterImage: item.image?,
+            characterName: item.name?,
+            characterStatus: item.status?,
+            characterLocation: item.location?.name,
+            characterFirstSeen: "Earth"
+        )
+
+//
+//        vc.characterName?.text = item.name ?? ""
+//        vc.characterStatus?.text = "\(item.status ?? "unknown") - \(item.species ??  "unknown")"
+//        vc.characterImage?.kf.setImage(with: URL(string: item.image ?? ""))
+//        vc.characterLocation?.text = item.location?.name ?? ""
+//        vc.characterFirstSeen?.text = "Earth"
+        print("push CharacterDetailViewController")
+        pushVC(controller: vc)
+
+        
+    }
+    func setLayout(){
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let width = Constants.screenWidth / 2 - 7.5
+        let height = 300
+        layout.minimumInteritemSpacing = 2.5
+        layout.minimumLineSpacing = 5
+        layout.itemSize.width = width
+        layout.itemSize.height = CGFloat(height)
+        
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == collectionView.numberOfItems(inSection: indexPath.section) - 1 {
+            updateNext()
         }
-      } else {return}
     }
-  }
-  func updateNext(){
-    ServiceManager.common.getMore(router: self.info.next, param: nil){
-      (response) in
-      if response?.results != nil {
-        guard let characters =  Mapper<RMCharacter>().mapArray(JSONObject: response?.results) else {return}
-        self.listCharacter.append(contentsOf: characters)
-        self.info = response?.info ?? Info()
-        DispatchQueue.main.async {
-          self.collectionView.reloadData()
+    func getFirstCharacters(){
+        ServiceManager.common.getCharacters(param: nil){
+            (response) in
+            if response?.results != nil {
+                guard let character =  Mapper<RMCharacter>().mapArray(JSONObject: response?.results) else {return}
+                self.listCharacter = character
+                self.info = response?.info ?? Info()
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            } else {return}
         }
-      } else {return}
     }
-    //requests another set of data (20 more items) from the server.
-    //      DispatchQueue.main.async(execute: collectionView.reloadData)
-  }
-
+    func updateNext(){
+        ServiceManager.common.getMore(router: self.info.next, param: nil){
+            (response) in
+            if response?.results != nil {
+                guard let characters =  Mapper<RMCharacter>().mapArray(JSONObject: response?.results) else {return}
+                self.listCharacter.append(contentsOf: characters)
+                self.info = response?.info ?? Info()
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            } else {return}
+        }
+        //requests another set of data (20 more items) from the server.
+        //      DispatchQueue.main.async(execute: collectionView.reloadData)
+    }
+    func pushVC(controller vc: UIViewController, animation anima: Bool = true) {
+        self.navigationController?.pushViewController(vc, animated: anima)
+    }
+    
 }
