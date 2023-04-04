c3d63b372cede5869c2948e7b91c67a2c9a39320
namnl <>
2023-04-04 23:55:08 +0700

    no message

---
+++ RickAndMorty/Extensions/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RickAndMorty
+//
+//  Created by namnl on 04/04/2023.
+//
+
+import Foundation
+import UIKit
+
+let screenWidth: CGFloat = UIScreen.main.bounds.width
+let screenHeight: CGFloat = UIScreen.main.bounds.height
