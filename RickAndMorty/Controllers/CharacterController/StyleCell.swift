f643b24464715a80a3a74d79ce82ca19c5b6a8e0
namnl <>
2023-04-07 18:11:12 +0700

    ok

---
+++ RickAndMorty/Controllers/CharacterController/StyleCell.swift
@@ -0,0 +1,23 @@
+//
+//  StyleCell.swift
+//  RickAndMorty
+//
+//  Created by cvcuong on 07/04/2023.
+//
+
+import UIKit
+
+class StyleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- RickAndMorty/Views/Character/NewVC.swift
+++ RickAndMorty/Views/Character/NewVC.swift
@@ -1,31 +1,20 @@
 //
-//  CharacterDetailViewController.swift
+//  NewVC.swift
 //  RickAndMorty
 //
-//  Created by namnl on 05/04/2023.
+//  Created by cvcuong on 07/04/2023.
 //
 
 import UIKit
 
-class CharacterDetailViewController: UIViewController,UINavigationControllerDelegate {
+class NewVC: UIViewController {
 
-    var item: RMCharacter = RMCharacter()
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.delegate = self
-        
+
         // Do any additional setup after loading the view.
     }
-    init(item: RMCharacter){
-        self.item = item
-        super.init(nibName: nil, bundle: nil)
 
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
 
     /*
     // MARK: - Navigation
